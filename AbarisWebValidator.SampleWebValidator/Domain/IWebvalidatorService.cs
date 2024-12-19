627f9057d17ba07b1144a901a77c62510fe5216a
EducationStoque <education@zeev.it>
2024-12-19 16:50:41 -0300

    Add files via upload

---
+++ AbarisWebValidator.SampleWebValidator/Domain/IWebvalidatorService.cs
@@ -0,0 +1,16 @@
+﻿using AbarisWebValidator.SampleWebValidator.Models;
+using CoreWCF;
+using CoreWCF.Web;
+using System.Collections.Generic;
+
+namespace AbarisWebValidator.SampleWebValidator.Domain
+{
+    [ServiceContract]
+    [ServiceKnownType(typeof(IndiceValor))]
+    public interface IWebvalidatorService
+    {
+        [OperationContract]
+        [WebGet(UriTemplate = "/getData_Banco?ID={id}")]
+        List<List<IndiceValor>> GetData_Banco(string id);
+    }
+}
